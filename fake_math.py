59c31855d33c320d226cd784a74f1651a0c0a9f5
Pavel-1572 <btcsapsan1972@gmail.com>
2024-10-21 06:15:56 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def divide(a, b):
+    if b == 0:
+        return 'Ошибка'
+    else:
+        return a / b
+
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,19 @@
+from math import inf
+
+
+def divide(a, b):
+    if b == 0:
+        return inf
+    else:
+        return a / b
+
+
+# from math import inf
+
+
+
+# def divide(first, second):
+#     if second == 0:
+#         return inf
+#     else:
+#         return first / second
